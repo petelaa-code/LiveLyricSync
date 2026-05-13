180bd74b37e833b9236cb1179447c2e4ba79e56f
petelaa-code <pete.laasonen@gmail.com>
2026-05-14 00:35:26 +0300

    Update websocket_server.py

--- server/websocket_server.py
+++ server/websocket_server.py
@@ -1,17 +1,24 @@
+import asyncio
 import json
+import websockets
 
-async def handler(self, websocket, path):
-    print("Client connected")
+class WebSocketServer:
+    def __init__(self, host="localhost", port=8765):
+        self.host = host
+        self.port = port
+        self.latest_data = None  # tänne main.py kirjoittaa viimeisimmän datan
 
-    try:
-        while True:
-            data = {
-                "type": "sync",
-                "position": 0.0,   # testiarvo
-                "word": None       # ei vielä oikeaa sanaa
-            }
+    def start(self):
+        return websockets.serve(self.handler, self.host, self.port)
 
-            await websocket.send(json.dumps(data))
-            await asyncio.sleep(0.1)
-    except websockets.exceptions.ConnectionClosed:
-        print("Client disconnected")
+    async def handler(self, websocket, path):
+        print("Client connected")
+
+        try:
+            while True:
+                if self.latest_data is not None:
+                    await websocket.send(json.dumps(self.latest_data))
+
+                await asyncio.sleep(0.05)  # 20 FPS
+        except websockets.exceptions.ConnectionClosed:
+            print("Client disconnected")
