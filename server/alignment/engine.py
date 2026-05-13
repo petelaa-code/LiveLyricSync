330c068e4c034bd1cc19bb78cfc9f27b93c2316e
petelaa-code <pete.laasonen@gmail.com>
2026-05-14 00:09:19 +0300

    Update engine.py

--- server/alignment/engine.py
+++ server/alignment/engine.py
@@ -1,1 +1,19 @@
+# Alignment Engine (placeholder)
+# Tämä moduuli tulee myöhemmin:
+# - vastaanottamaan audiokehyksiä
+# - vertaamaan niitä lyriikoihin
+# - laskemaan missä kohtaa laulua ollaan
+# - palauttamaan synkronointitiedon
+
+class AlignmentEngine:
+    def __init__(self):
+        pass
+
+    def load_lyrics(self, lyrics: str):
+        # TODO: esikäsittele lyriikat
+        pass
+
+    def process_frame(self, frame):
+        # TODO: analysoi audiokehys ja palauta synkka
+        return None
 
