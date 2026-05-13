4b0c7452fe30bccca6f642c25c4629afa3367a54
petelaa-code <pete.laasonen@gmail.com>
2026-05-14 00:13:50 +0300

    Update websocket_server.py

--- server/websocket_server.py
+++ server/websocket_server.py
@@ -1,1 +1,28 @@
+# WebSocket server (placeholder)
+# Tämä moduuli tulee myöhemmin:
+# - lähettämään synkronointidataa frontille
+# - vastaanottamaan komentoja frontilta
+# - pitämään yhteyden auki reaaliajassa
+
+import asyncio
+import websockets
+
+class WebSocketServer:
+    def __init__(self, host="localhost", port=8765):
+        self.host = host
+        self.port = port
+
+    async def handler(self, websocket, path):
+        print("Client connected")
+
+        try:
+            while True:
+                # TODO: lähetä synkronointidataa
+                await asyncio.sleep(0.1)
+        except websockets.exceptions.ConnectionClosed:
+            print("Client disconnected")
+
+    def start(self):
+        print(f"Starting WebSocket server on ws://{self.host}:{self.port}")
+        return websockets.serve(self.handler, self.host, self.port)
 
