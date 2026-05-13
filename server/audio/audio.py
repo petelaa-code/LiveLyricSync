a21d25b7706f21a4765299db5657394aa210c3f3
petelaa-code <pete.laasonen@gmail.com>
2026-05-14 00:30:37 +0300

    Update audio.py

--- server/audio/audio.py
+++ server/audio/audio.py
@@ -1,14 +1,16 @@
+import time
+
 # Audio Input Layer (placeholder)
-# This module will later handle:
+# Tämä moduuli tulee myöhemmin käsittelemään:
 # - WASAPI / ASIO audio capture
-# - Buffering
-# - Downmixing to mono
-# - Normalization
-# - Frame generator for alignment engine
+# - Puskurointi
+# - Downmix mono
+# - Normalisointi
+# - Kehysten generointi alignment engineä varten
 
 class AudioInput:
     def __init__(self):
-        pass
+        self.frame_index = 0
 
     def start(self):
         # TODO: Initialize audio device
@@ -17,3 +19,13 @@ class AudioInput:
     def read_frame(self):
         # TODO: Return PCM audio frame
         return None
+
+    def get_frame(self):
+        # Testikehys, jotta pipeline voidaan rakentaa
+        frame = {
+            "index": self.frame_index,
+            "timestamp": time.time(),
+            "data": None  # ei vielä oikeaa audiota
+        }
+        self.frame_index += 1
+        return frame
